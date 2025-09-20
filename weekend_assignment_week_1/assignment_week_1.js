df0d8edfae98291badb71f0eb7ae7e4f07ceaa8c
shivang singh <90271831+Fatyoda3@users.noreply.github.com>
2025-09-20 20:02:47 +0530

    Update assignment_week_1.js
    I have been able to push forward and find the "L" in the string of "Jungle".

--- weekend_assignment_week_1/assignment_week_1.js
+++ weekend_assignment_week_1/assignment_week_1.js
@@ -3,4 +3,11 @@ const space = Jungle.length;
 
 console.log(Jungle ,"space available : ", space);
 
-console.log(space);
+let LionFound = false;
+
+for (let i = 0; i < space; i++) {
+    const currentPos = i;
+    const scan = Jungle[currentPos]
+    if(scan === 'L') LionFound = true;
+}
+console.log("Lion found " , LionFound);
