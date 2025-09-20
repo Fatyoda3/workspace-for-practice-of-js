93280baf96877b48caaffb9f60b979356a88c5df
shivang singh <90271831+Fatyoda3@users.noreply.github.com>
2025-09-20 20:09:51 +0530

    Update assignment_week_1.js
    I can count and track how many steps the lion will take .

--- weekend_assignment_week_1/assignment_week_1.js
+++ weekend_assignment_week_1/assignment_week_1.js
@@ -1,17 +1,19 @@
 const Jungle = " L  Z";
 const space = Jungle.length;
 
-console.log(Jungle ,"space available : ", space);
+console.log(Jungle, "space available : ", space);
 
 let LionFound = false;
+let count = 0;
 
 for (let i = 0; i < space; i++) {
-    const currentPos = i;
-    const scan = Jungle[currentPos];
-    
-    if(scan === 'L') LionFound = true;  
-    if(LionFound && scan === ' '){
-    console.log('step taken by the lion ');
-    }
+  const currentPos = i;
+  const scan = Jungle[currentPos];
+  if (scan === "L") LionFound = true;
+
+  if (LionFound && scan === " ") {
+    console.log("step taken by the lion ");
+    count = count + 1;
+  }
 }
-console.log("Lion found " , LionFound);
+console.log("Lion found is", LionFound, "and can hunt after ", count, "steps");
