65486c6744a002dfe2d2e8da7c9ec6c3b6d00ede
shivang singh <90271831+Fatyoda3@users.noreply.github.com>
2025-09-20 20:06:55 +0530

    Update assignment_week_1.js
    Now I can count if the lion has space to move forward towards the hunt

--- weekend_assignment_week_1/assignment_week_1.js
+++ weekend_assignment_week_1/assignment_week_1.js
@@ -7,7 +7,11 @@ let LionFound = false;
 
 for (let i = 0; i < space; i++) {
     const currentPos = i;
-    const scan = Jungle[currentPos]
-    if(scan === 'L') LionFound = true;
+    const scan = Jungle[currentPos];
+    
+    if(scan === 'L') LionFound = true;  
+    if(LionFound && scan === ' '){
+    console.log('step taken by the lion ');
+    }
 }
 console.log("Lion found " , LionFound);
