e4bf2a890fc16b63fdb0cf00077242e35052dc70
shivang singh <90271831+Fatyoda3@users.noreply.github.com>
2025-09-21 11:52:35 +0530

    Create another_solution.js
    I'm working on a better solution if possible

---
+++ weekend_assignment_week_1/another_solution.js
@@ -0,0 +1,24 @@
+const jungle = "    LLZL L  ";
+let previousAnimal = "";
+let steps = -1;
+let count = 0;
+
+for (let i = 0; i < jungle.length; i++) {
+  const current = jungle[i];
+  if (current === " " && previousAnimal) {
+    count = count + 1;
+  } else if (current !== " ") {
+    if (previousAnimal !== current && previousAnimal !== "") {
+
+      const changeSteps = steps === -1 || steps > count;
+
+      steps = changeSteps ? count : steps;
+
+      count = 0;
+    } else if (previousAnimal === current && previousAnimal !== "") {
+      count = 0;
+    }
+    previousAnimal = current;
+  }
+}
+console.log("value of steps", steps);
