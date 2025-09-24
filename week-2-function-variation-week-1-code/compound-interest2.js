52d0c0f785ba6da0a377af532583bfe8d44208b9
shivang singh <90271831+Fatyoda3@users.noreply.github.com>
2025-09-24 17:51:17 +0530

    Create compound-interest2.js

---
+++ week-2-function-variation-week-1-code/compound-interest2.js
@@ -0,0 +1,41 @@
+const p = 100;
+const t = 2;
+const r = 10;
+
+let amount = p;
+function interestPerYear(amount, r) {
+  return amount * (r / 100);
+}
+
+function getCompound(x, t, r) {
+  let amount = x;
+
+  for (let currentYear = 0; currentYear < t; currentYear++) {
+    amount = amount + interestPerYear(amount, r);
+  }
+
+  return amount - x;
+}
+
+function tesCompoundInterest(amount, t, r, expectedValue) {
+  const valueWeGot = getCompound(amount, t, r);
+  let isWorking;
+  let delta = (expectedValue - valueWeGot);
+  if (-0.9 < delta && delta < 0.9) {
+    isWorking = '✅';
+  }
+  else
+    isWorking = valueWeGot === expectedValue ? '✅' : '❌';
+  const message = "value we expected " + expectedValue + " and we got " + valueWeGot + ' ' + isWorking;
+  console.log(message);
+}
+
+function main() {
+  tesCompoundInterest(p, t, r, 21);
+  tesCompoundInterest(100, 3, 10, 33.0);
+  tesCompoundInterest(p, 4, 10, 46.41);
+  tesCompoundInterest(p, 5, 10, 61.0);
+  tesCompoundInterest(p, 6, 10, 77.1);
+}
+
+main();
