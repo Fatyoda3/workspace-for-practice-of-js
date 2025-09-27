8fc5a2ebfa45babce1a56cf81e404334725c8228
shivang singh <90271831+Fatyoda3@users.noreply.github.com>
2025-09-27 16:01:39 +0530

    Update solution.js
    a failing test cases

--- --weekend-assignment/assignment-week-2/solution.js
+++ --weekend-assignment/assignment-week-2/solution.js
@@ -6,12 +6,55 @@ abyss	ab,y,s,s
 this	tis,h
  */
 
-const text = 'apple';
+const string = 'apple';
 
-function splitByVowel(string) {
-  return 'ape,p,l,';
+function isVowel(letter) {
+  switch (letter) {
+    case 'a':
+      return true;
+
+    case 'e':
+      return true;
+
+    case 'i':
+      return true;
+
+    case 'o':
+      return true;
+
+    case 'u':
+      return true;
+  }
+  return false;
 }
 
+function splitByVowel(string) {
+  let editedString = '';
+  let currentWord = string[0];
+  let c = 1;
+  if (isVowel(string[0])) {
+    currentWord = '';
+    c = 0
+  }
+  let prev = '';
+  while (c < string.length) {
+
+    if (isVowel(string[c]) && !isVowel(prev)) {
+      currentWord += string[c];
+      prev = string[c];
+    }
+    else if (!isVowel(string[c]) && isVowel(prev)) {
+      currentWord += string[c];
+      prev = string[c];
+    }
+    else {
+      editedString += string[c] + ',';
+    }
+    c++;
+  }
+  console.log(editedString);
+  return currentWord + ',' + editedString;
+}
 function composeMessage(string, expectedValue, valueWeGot) {
   const part1 = " string->" + string;
   const part2 = " expectedValue->" + expectedValue;
@@ -24,12 +67,21 @@ function testSplitByVowel(string, expectedValue) {
   const valueWeGot = splitByVowel(string);
   const checkIfWorks = valueWeGot === expectedValue ? '✅' : '❌';
   const message = composeMessage(string, expectedValue, valueWeGot);
-  
   console.log(checkIfWorks, message);
+
 }
 
 function testAll() {
-  testSplitByVowel(text, 'ape,p,l,');
+
+  // testSplitByVowel('abba', 'aba,b,');
+  // testSplitByVowel('apple', 'ape,p,l,');
+  // testSplitByVowel('applee', 'ape,p,l,e,');
+  // testSplitByVowel('there', 'tere,h,');
+  // testSplitByVowel('hello', 'helo,l,');
+  // testSplitByVowel('this', 'tis,h,');
+  // testSplitByVowel('abyss', 'ab,y,s,s,');
+  testSplitByVowel('aaabbb', 'ab,ab,ab,');
+  // testSplitByVowel('aaaeee', 'a,a,a,e,e,e,');
 }
 
 testAll();
