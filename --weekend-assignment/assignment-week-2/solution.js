6ffa7bad73a8f7fccdd2cda9cd15f1fb270d753b
shivang singh <90271831+Fatyoda3@users.noreply.github.com>
2025-09-27 16:56:38 +0530

    Update solution.js

--- --weekend-assignment/assignment-week-2/solution.js
+++ --weekend-assignment/assignment-week-2/solution.js
@@ -4,7 +4,7 @@ there	tere,h
 hello	helo,l
 abyss	ab,y,s,s
 this	tis,h
- */
+*/
 
 const string = 'apple';
 
@@ -12,16 +12,12 @@ function isVowel(letter) {
   switch (letter) {
     case 'a':
       return true;
-
     case 'e':
       return true;
-
     case 'i':
       return true;
-
     case 'o':
       return true;
-
     case 'u':
       return true;
   }
@@ -32,33 +28,34 @@ function splitByVowel(string) {
   let editedString = '';
   let currentWord = string[0];
   let c = 1;
+
   if (isVowel(string[0])) {
     currentWord = '';
-    c = 0
+    c = 0;
   }
-  let prev = '';
-  while (c < string.length) {
 
-    if (isVowel(string[c]) && !isVowel(prev)) {
-      currentWord += string[c];
-      prev = string[c];
-    }
-    else if (!isVowel(string[c]) && isVowel(prev)) {
-      currentWord += string[c];
-      prev = string[c];
-    }
-    else {
-      editedString += string[c] + ',';
+  let prev = '';
+
+  while (c < string.length) {
+    const letter = string[c];
+    const currentVowel = isVowel(letter) && !isVowel(prev);
+    const previousVowel = !isVowel(letter) && isVowel(prev);
+
+    if (currentVowel || previousVowel) {
+      currentWord += letter;
+      prev = letter;
+    } else {
+      editedString += letter + ',';
     }
     c++;
   }
-  console.log(editedString);
   return currentWord + ',' + editedString;
 }
+
 function composeMessage(string, expectedValue, valueWeGot) {
-  const part1 = " string->" + string;
-  const part2 = " expectedValue->" + expectedValue;
-  const part3 = " valueWeGot->  " + valueWeGot;
+  const part1 = " in->" + string;
+  const part2 = " ex->" + expectedValue;
+  const part3 = " out->" + valueWeGot;
 
   return part1 + part2 + '|' + part3;
 }
@@ -73,15 +70,16 @@ function testSplitByVowel(string, expectedValue) {
 
 function testAll() {
 
-  // testSplitByVowel('abba', 'aba,b,');
-  // testSplitByVowel('apple', 'ape,p,l,');
-  // testSplitByVowel('applee', 'ape,p,l,e,');
-  // testSplitByVowel('there', 'tere,h,');
-  // testSplitByVowel('hello', 'helo,l,');
-  // testSplitByVowel('this', 'tis,h,');
-  // testSplitByVowel('abyss', 'ab,y,s,s,');
   testSplitByVowel('aaabbb', 'ab,ab,ab,');
-  // testSplitByVowel('aaaeee', 'a,a,a,e,e,e,');
+  testSplitByVowel('aaaeee', 'a,a,a,e,e,e,');
+  testSplitByVowel("there", "tere,h,");
+  testSplitByVowel("hello", "helo,l,");
+  testSplitByVowel("abyss", "ab,y,s,s,");
+  testSplitByVowel("this", "tis,h,");
+  testSplitByVowel("cat", "cat,");
+  testSplitByVowel("boat", "bot,a,");
+  testSplitByVowel("applee", "ape,p,l,e,");
+  testSplitByVowel("rhythm", "r,h,y,t,h,m,");
 }
 
 testAll();
