86b0d46e4c278e89dc7fe17e847071e0e2495a46
shivang singh <90271831+Fatyoda3@users.noreply.github.com>
2025-09-28 09:35:02 +0530

    Update split_by_vowel.js
    just added some really important changes

--- --weekend-assignment/assignment-week-2/split_by_vowel.js
+++ --weekend-assignment/assignment-week-2/split_by_vowel.js
@@ -9,62 +9,72 @@ function isVowel(letter) {
   }
   return false;
 }
+function isAlternate(current, last) {
+  return isVowel(current) !== isVowel(last);
+}
 
 function splitByVowel(string) {
-  let editedString = '';
-  let currentWord = string[0];
-  let c = 1;
+  let wordBuffer = '';
+  let unusedLetters = '';
+  let reducedString = string;
+  let result = '';
 
-  if (isVowel(string[0])) {
-    currentWord = '';
-    c = 0;
-  }
+  while (reducedString !== '') {
 
-  let prev = '';
+    wordBuffer = reducedString[0];
 
-  while (c < string.length) {
-    const letter = string[c];
-    const currentVowel = isVowel(letter) && !isVowel(prev);
-    const previousVowel = !isVowel(letter) && isVowel(prev);
+    for (let current = 1; current < reducedString.length; current++) {
 
-    if (currentVowel || previousVowel) {
-      currentWord += letter;
-      prev = letter;
-    } else {
-      editedString += letter + ',';
+      if (isAlternate(reducedString[current], wordBuffer[wordBuffer.length - 1])) {
+
+        wordBuffer += reducedString[current];
+
+      } else {
+        unusedLetters += reducedString[current];
+      }
     }
-    c++;
+
+    result += wordBuffer + ',';
+    reducedString = unusedLetters;
+    unusedLetters = '';
+
   }
-  return currentWord + ',' + editedString;
+
+  return result;
 }
 
 function composeMessage(string, expectedValue, valueWeGot) {
-  const part1 = " in->" + string;
-  const part2 = " ex->" + expectedValue;
-  const part3 = " out->" + valueWeGot;
+  const input = " input- " + string;
+  const expected = " expected- " + expectedValue;
+  const output = " output- " + valueWeGot;
 
-  return part1 + part2 + '|' + part3;
+  return input + expected + '|' + output;
 }
 
 function testSplitByVowel(string, expectedValue) {
-  const valueWeGot = splitByVowel(string);
+  const valueWeGot = splitByVowel(string, 0);
   const checkIfWorks = valueWeGot === expectedValue ? '✅' : '❌';
   const message = composeMessage(string, expectedValue, valueWeGot);
+
   console.log(checkIfWorks, message);
 
 }
 
 function testAll() {
 
-  testSplitByVowel('aaabbb', 'ab,ab,ab,');
+  console.log('working test cases !');
+  testSplitByVowel("aaabbb", "ab,ab,ab,");
+  testSplitByVowel('tools', 'tol,os,');
+  testSplitByVowel('poetry', 'pot,er,y,');
+
   testSplitByVowel('aaaeee', 'a,a,a,e,e,e,');
-  testSplitByVowel("there", "tere,h,");
   testSplitByVowel("hello", "helo,l,");
   testSplitByVowel("abyss", "ab,y,s,s,");
   testSplitByVowel("this", "tis,h,");
+
   testSplitByVowel("cat", "cat,");
   testSplitByVowel("boat", "bot,a,");
-  testSplitByVowel("applee", "ape,p,l,e,");
+  testSplitByVowel("applee", "ape,pe,l,");
   testSplitByVowel("rhythm", "r,h,y,t,h,m,");
 }
 
